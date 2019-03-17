cad61b4c07ab1a0bf5ba9e69c3071e43b4bb2daa
akshaya1 <akshayanelliat23@gmail.com>
2019-03-17 20:05:30 +0000

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblIPAddress = new System.Windows.Forms.Label();
+            this.lblPortNo = new System.Windows.Forms.Label();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.txtPortNo = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSelectFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(399, 103);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(98, 28);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblIPAddress
+            // 
+            this.lblIPAddress.AutoSize = true;
+            this.lblIPAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIPAddress.Location = new System.Drawing.Point(243, 181);
+            this.lblIPAddress.Name = "lblIPAddress";
+            this.lblIPAddress.Size = new System.Drawing.Size(74, 16);
+            this.lblIPAddress.TabIndex = 2;
+            this.lblIPAddress.Text = "IP Address";
+            // 
+            // lblPortNo
+            // 
+            this.lblPortNo.AutoSize = true;
+            this.lblPortNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPortNo.Location = new System.Drawing.Point(243, 240);
+            this.lblPortNo.Name = "lblPortNo";
+            this.lblPortNo.Size = new System.Drawing.Size(53, 16);
+            this.lblPortNo.TabIndex = 3;
+            this.lblPortNo.Text = "Port No";
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIPAddress.Location = new System.Drawing.Point(399, 176);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(130, 22);
+            this.txtIPAddress.TabIndex = 4;
+            this.txtIPAddress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPortNo
+            // 
+            this.txtPortNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPortNo.Location = new System.Drawing.Point(399, 240);
+            this.txtPortNo.Name = "txtPortNo";
+            this.txtPortNo.Size = new System.Drawing.Size(130, 22);
+            this.txtPortNo.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(340, 319);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSelectFile
+            // 
+            this.txtSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSelectFile.Location = new System.Drawing.Point(246, 111);
+            this.txtSelectFile.Name = "txtSelectFile";
+            this.txtSelectFile.Size = new System.Drawing.Size(100, 22);
+            this.txtSelectFile.TabIndex = 7;
+            this.txtSelectFile.TextChanged += new System.EventHandler(this.txtSelectFile_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSelectFile);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPortNo);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.lblPortNo);
+            this.Controls.Add(this.lblIPAddress);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblIPAddress;
+        private System.Windows.Forms.Label lblPortNo;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.TextBox txtPortNo;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSelectFile;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private static string shortFileName = "";
+        private static string fileName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "File Sharing Client";
+            dlg.ShowDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+
+                txtSelectFile.Text = dlg.FileName;
+                fileName = dlg.FileName;
+                shortFileName = dlg.SafeFileName;
+            }
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string ipAddress = txtIPAddress.Text;
+            int port = int.Parse(txtPortNo.Text);
+            string fileName = txtSelectFile.Text;
+            Task.Factory.StartNew(() => SendFile(ipAddress, port,
+
+             fileName, shortFileName));
+
+            MessageBox.Show("File Sent");
+        }
+        public void SendFile(string remoteHostIP, int remoteHostPort
+
+           , string longFileName, string shortFileName)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(remoteHostIP))
+                {
+                    byte[] fileNameByte = Encoding.ASCII.GetBytes
+
+                    (shortFileName);
+                    byte[] fileData = File.ReadAllBytes(longFileName);
+                    byte[] clientData = new byte[4 + fileNameByte.Length
+
+                       + fileData.Length];
+                    byte[] fileNameLen = BitConverter.GetBytes(
+
+                     fileNameByte.Length);
+                    fileNameLen.CopyTo(clientData, 0);
+                    fileNameByte.CopyTo(clientData, 4);
+                    fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+                    TcpClient clientSocket = new TcpClient(remoteHostIP,
+
+                     remoteHostPort);
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Write(clientData, 0, clientData.GetLength
+
+                     (0));
+                    networkStream.Close();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txtSelectFile_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
